0b2fe488a90cc63c14eff61d1f8cda0cbaf7553d
alpbourne <alpbourne@gmail.com>
2017-12-05 17:35:55 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params)
@@ -26,4 +26,10 @@ class PostsController < ApplicationController
 	def edit
 	  @post = Post.find(params[:id])
 	end
+end
+
+private
+ 
+def post_params
+  params.require(:post).permit(:title, :description)
 end
